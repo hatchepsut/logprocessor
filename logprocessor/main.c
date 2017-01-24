43d947cc2431423d5c897f87ebadb72f8831b8e3
peter <peter.soderlind@gmail.com>
2017-01-24 20:11:53 +0100

    Initial Commit

---
+++ logprocessor/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  logprocessor
+//
+//  Created by Peter Söderlind on 2017-01-24.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
